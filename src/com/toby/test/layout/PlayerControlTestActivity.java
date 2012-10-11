0a95ff104aa33599ff6dba00583bf4863352516c
tyhs1414 <tyhs1414@gmail.com>
2012-10-11 09:50:25 +0800

    Add 'PlayerControl' test

---
+++ src/com/toby/test/layout/PlayerControlTestActivity.java
@@ -0,0 +1,16 @@
+package com.toby.test.layout;
+
+import com.toby.test.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayerControlTestActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.player_control);
+	}
+
+}
